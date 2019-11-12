47a8198a86f50c0334c8a482714efd09587ed270
ktsb3 <38196743+ktsb3@users.noreply.github.com>
2019-11-11 23:39:11 -0600

    Main menu and levels
    Added:
    title screen
    Main menu
    level select
    tutorial level
    level 1

---
+++ Saving Sound/Assets/Scripts/Handler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Handler : MonoBehaviour{ 
+    public string scene;
+    public Button button;
+    public Button button2;
+    
+    void Start(){
+
+    }
+
+    void Update(){
+
+    }
+
+    public void Tutorial(string scene){
+        Application.LoadLevel("Tutorial2");
+    }
+
+    public void Level1(string scene){
+        Application.LoadLevel("Level1");
+    }
+}
---
+++ Saving Sound/Assets/Scripts/menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour{
+    public string scene;
+    private bool loadLock;
+
+    void Start(){
+
+    }
+
+    void Update(){
+        if(Input.GetMouseButtonDown(0) && !loadLock){
+            LoadScene();
+        }
+    }
+
+    void LoadScene(){
+        loadLock = true;
+        Application.LoadLevel(scene);
+    }
+}
