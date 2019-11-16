5fdf9f2c2bd9a75f248cf1af530009b2e5b43e02
CoolingBroski <camarenapliego@gmail.com>
2019-11-16 15:22:32 -0600

    Added object behaviour and physics enhancements

--- Saving Sound/Assets/Scripts/KeyboardInput.cs
+++ Saving Sound/Assets/Scripts/KeyboardInput.cs
@@ -19,25 +19,33 @@ public class KeyboardInput : MonoBehaviour {
         vel = GetComponent<Rigidbody2D>().velocity;
         forceX = -vel[0]*0.5f; // Detener al jugador cuando no hay input de izquierda o derecha
         if (Input.GetKey("right")){
-            if (vel[0] < 0)
+            if (vel[0] < 0 && GetComponent<StatsTracker>().jump == true)
             {
                 vel[0] = 0f;
             }
             forceX = velocityX;
+            if (GetComponent<StatsTracker>().jump == false)
+            {
+                forceX = forceX/4f;
+            }
         }
         if (Input.GetKey("left")){
-            if (vel[0] > 0)
+            if (vel[0] > 0 && GetComponent<StatsTracker>().jump == true)
             {
                 vel[0] = 0f;
             }
             forceX = -velocityX;
+            if (GetComponent<StatsTracker>().jump == false)
+            {
+                forceX = forceX / 4f;
+            }
         }
         if (Input.GetKey("space") && GetComponent<StatsTracker>().jump == true)
         {
             if(vel[1] < 4.8f){
                 forceY = velocityY;
             }
-            print(vel);
+            //print(vel);
             GetComponent<StatsTracker>().jump = false;
         }
         GetComponent<Rigidbody2D>().velocity = vel;
--- Saving Sound/Assets/Scripts/StatsTracker.cs
+++ Saving Sound/Assets/Scripts/StatsTracker.cs
@@ -6,23 +6,81 @@ public class StatsTracker : MonoBehaviour
 {
     public bool jump;
     public int lives;
+    public int score;
+    public GameObject checkpoint;
+    private Vector3 init_pos;
+    public bool reached_checkpoint;
     void Start()
     {
         jump = true;
+        lives = 3;
+        score = 0;
+        reached_checkpoint = false;
+        init_pos = gameObject.transform.position;
     }
     void Update()
     {
+        //print(score);
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision) //nPara objetos con que debe interactuar fisicamente
     {
-        var relativePosition = transform.InverseTransformPoint(collision.transform.position);
-        if(relativePosition.y < 0) // Detectar que la colision con el collider por debajo
+        if (collision.gameObject.tag == "Destroyer")
         {
-            jump = true;
+            lives--;
+            if (lives == 0)
+            {
+                Destroy(gameObject);
+            }
+            if (reached_checkpoint == true)
+            {
+                gameObject.transform.position = checkpoint.transform.position;
+            }
+            else
+            {
+                gameObject.transform.position = init_pos;
+            }
+
+        }
+        else
+        {
+            var relativePosition = transform.InverseTransformPoint(collision.transform.position);
+            //print(relativePosition.y);
+            if (relativePosition.y < -4f) // Detectar que la colision con el collider por debajo
+            {
+                jump = true;
+            }
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
+        //if (collision.gameObject.tag != "DoubleJump")
+        //{
+        //    jump = false;
+        //}
         jump = false;
     }
+    private void OnTriggerEnter2D(Collider2D collision) // Debe ser un Trigger para que no haya interaccion fisica con objetos de este estilo
+    {
+        if (collision.gameObject.tag == "Heart")
+        {
+            lives++;
+        }
+        else if(collision.gameObject.tag == "DoubleJump")
+        {
+            jump = true;
+        }
+        else if(collision.gameObject.tag == "Score")
+        {
+            score = score + 100;
+        }
+        else if(collision.gameObject.tag == "Checkpoint")
+        {
+            reached_checkpoint = true;
+        }
+        if (collision.gameObject.tag != "Checkpoint" && collision.gameObject.tag != "DoubleJump")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
+
