a5da346bcc2f09e04a13cdd0ea8d4c7698c418b5
stephanydvm <38016681+stephanydvm@users.noreply.github.com>
2019-11-17 00:39:50 -0600

    Added objects to levels

---
+++ Saving Sound/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+
+        if (collider.gameObject.tag == "Player")
+        {
+            print("CHOQUE");
+            if (gameObject.GetComponent<AudioSource>().GetComponent<AudioSource>())
+            {
+                
+                gameObject.GetComponent<AudioSource>().GetComponent<AudioSource>().Play();
+            }
+            
+
+
+        }
+
+
+    }
+
+
+}
